9d5baaf3147536dcbc2ce2f810de973ad3480d48
Savinu Nugith Dulwan Wimalasuriya <150595620+stu0320330@users.noreply.github.com>
2023-11-13 17:09:01 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # # # # #
+        . . . . #
+        # # # # #
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # . # . #
+        # . # . #
+        # . # . #
+        # . # . #
+        # # # # #
+        `)
+})
